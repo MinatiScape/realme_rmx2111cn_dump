b641b68e96a8842697e8909a0b6737a2ea11284e
root <root@fv-az127-704.h3uyzbqov44edfyrgkjongjtha.bx.internal.cloudapp.net>
2021-06-20 08:55:23 +0000

    Add extras for full_oppo6853-user 10 QP1A.190711.020 p2tc16sprmpr2V1 release-keys
    Signed-off-by: root <root@fv-az127-704.h3uyzbqov44edfyrgkjongjtha.bx.internal.cloudapp.net>

---
+++ README.md
@@ -0,0 +1,21 @@
+## full_oppo6853-user 10 QP1A.190711.020 p2tc16sprmpr2V1 release-keys
+- Manufacturer: realme
+- Platform: mt6853
+- Codename: RMX2111CN
+- Brand: realme
+- Flavor: full_oppo6853-user
+- Release Version: 10
+- Id: QP1A.190711.020
+- Incremental: 0310_202102072214
+- Tags: release-keys
+- CPU Abilist: arm64-v8a,armeabi-v7a,armeabi
+- A/B Device: false
+- Locale: zh-CN
+- Screen Density: 480
+- Fingerprint: 
+- OTA version: RMX2111_11.A.31_0310_202102072214
+- Branch: RMX2111_11.A.31_0310_202102072214
+- Repo: realme_rmx2111cn_dump
+
+
+>Dumped by [Phoenix Firmware Dumper](https://github.com/DroidDumps/phoenix_firmware_dumper)
---
+++ join_split_files.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+cat system/system/preload/Gamecenter/Gamecenter.apk.* 2>/dev/null >> system/system/preload/Gamecenter/Gamecenter.apk
+rm -f system/system/preload/Gamecenter/Gamecenter.apk.* 2>/dev/null
+cat system/system/preload/SmartHome/SmartHome.apk.* 2>/dev/null >> system/system/preload/SmartHome/SmartHome.apk
+rm -f system/system/preload/SmartHome/SmartHome.apk.* 2>/dev/null
+cat system/system/preload/OppoVideoEditor/OppoVideoEditor.apk.* 2>/dev/null >> system/system/preload/OppoVideoEditor/OppoVideoEditor.apk
+rm -f system/system/preload/OppoVideoEditor/OppoVideoEditor.apk.* 2>/dev/null
+cat system/system/preload/realmeLink/realmeLink.apk.* 2>/dev/null >> system/system/preload/realmeLink/realmeLink.apk
+rm -f system/system/preload/realmeLink/realmeLink.apk.* 2>/dev/null
+cat system/system/preload/OPPOStore/OPPOStore.apk.* 2>/dev/null >> system/system/preload/OPPOStore/OPPOStore.apk
+rm -f system/system/preload/OPPOStore/OPPOStore.apk.* 2>/dev/null
+cat system/system/priv-app/KeKeThemeStore/KeKeThemeStore.apk.* 2>/dev/null >> system/system/priv-app/KeKeThemeStore/KeKeThemeStore.apk
+rm -f system/system/priv-app/KeKeThemeStore/KeKeThemeStore.apk.* 2>/dev/null
+cat system/system/priv-app/OppoGallery2/OppoGallery2.apk.* 2>/dev/null >> system/system/priv-app/OppoGallery2/OppoGallery2.apk
+rm -f system/system/priv-app/OppoGallery2/OppoGallery2.apk.* 2>/dev/null
+cat system/system/priv-app/Settings/Settings.apk.* 2>/dev/null >> system/system/priv-app/Settings/Settings.apk
+rm -f system/system/priv-app/Settings/Settings.apk.* 2>/dev/null
+cat system/system/priv-app/HeyTapSpeechAssist/HeyTapSpeechAssist.apk.* 2>/dev/null >> system/system/priv-app/HeyTapSpeechAssist/HeyTapSpeechAssist.apk
+rm -f system/system/priv-app/HeyTapSpeechAssist/HeyTapSpeechAssist.apk.* 2>/dev/null
+cat system/system/priv-app/Browser/Browser.apk.* 2>/dev/null >> system/system/priv-app/Browser/Browser.apk
+rm -f system/system/priv-app/Browser/Browser.apk.* 2>/dev/null
+cat system/system/priv-app/SystemUI/SystemUI.apk.* 2>/dev/null >> system/system/priv-app/SystemUI/SystemUI.apk
+rm -f system/system/priv-app/SystemUI/SystemUI.apk.* 2>/dev/null
+cat system/system/product/priv-app/GmsCore/GmsCore.apk.* 2>/dev/null >> system/system/product/priv-app/GmsCore/GmsCore.apk
+rm -f system/system/product/priv-app/GmsCore/GmsCore.apk.* 2>/dev/null
+cat system/system/product/app/WebViewGoogleNX/WebViewGoogleNX.apk.* 2>/dev/null >> system/system/product/app/WebViewGoogleNX/WebViewGoogleNX.apk
+rm -f system/system/product/app/WebViewGoogleNX/WebViewGoogleNX.apk.* 2>/dev/null
+cat system/system/app/OppoCamera/OppoCamera.apk.* 2>/dev/null >> system/system/app/OppoCamera/OppoCamera.apk
+rm -f system/system/app/OppoCamera/OppoCamera.apk.* 2>/dev/null
+cat system/system/app/Instant/Instant.apk.* 2>/dev/null >> system/system/app/Instant/Instant.apk
+rm -f system/system/app/Instant/Instant.apk.* 2>/dev/null
